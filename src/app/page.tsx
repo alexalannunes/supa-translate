5d6b8a107e828b6bbb814ddb146db18e8f1f709b
Alex Alan Nunes <alexalannunes@gmail.com>
2025-09-20 00:54:11 -0300

    setup projection and envs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
+"use client";
 import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
+          className="dark:invert size-auto"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
         />
+        <button
+          onClick={() => {
+            fetch("/api/demo")
+              .then((e) => e.json())
+              .then(console.log);
+          }}
+        >
+          call
+        </button>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
@@ -33,7 +42,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
+              className="dark:invert size-auto"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
