e9c8a47571353176c89145452c6db89b88e23ab2
Alex Alan Nunes <alexalannunes@gmail.com>
2025-09-20 11:55:17 -0300

    fix: add exclamation mark to the "App init" heading for emphasis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="font-sans p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h2>App init</h2>
+        <h2>App init!</h2>
       </main>
     </div>
   );
